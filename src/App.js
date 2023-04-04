e729f55807cef3f566400f8169eab10d3b06759a
Laert <wowgiona49@gmail.com>
2023-04-04 23:55:22 +0200

    home implemented with mock

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
+import client from "./graphql/client";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserProfile from "./pages/UserProfile";
+import Recommendations from "./pages/Recommendations";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import './App.css'
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        {/* <Banner /> */}
+        <Navbar />
+          <Routes>
+            <Route index path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<UserProfile />} />
+            <Route path="/recommendations" element={<Recommendations />} />
+          </Routes>
+      </Router>
     </div>
+    </ApolloProvider>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {/* Add more content and logic for the Login page */}
+    </div>
+  );
+};
+
+export default Login;
